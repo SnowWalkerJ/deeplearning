fd543944cde4bf00562b14c5b90ab907dc5d52e9
SnowWalkerJ <jike3212001@163.com>
2016-10-23 11:01:01 +0800

    tensorboard

--- cat-dog/train.py
+++ cat-dog/train.py
@@ -1,12 +1,17 @@
 from tf_learn.utils.data_utils import LazyImageData, BigData
 from net import Model
+import os
 
+
+logdir = 'logs'
 
 if __name__ == '__main__':
     data = BigData('dump')
     # data = LazyImageData('data/train', lambda name: 1 if name.split('.')[0] == 'cat' else 0)
     # data.set_crop((300, 300))
-    model = Model()
+    if not os.path.exists(logdir):
+        os.mkdir(logdir)
+    model = Model(logdir)
     placeholders = {
         'keep_prob': {
             'train': 0.5,
