f2d95e7759d31806f2ff0c31171c1ef0267cbee3
SnowWalkerJ <jike3212001@163.com>
2016-10-23 18:45:47 +0800

    .

--- cat-dog/net.py
+++ cat-dog/net.py
@@ -10,28 +10,37 @@ class Model(tf_learn.models.dnn.DNN):
                 'train': 0.5,
                 'evaluate': 1.0,
             },
-            'lr': 1e-2
+            'lr': 1e-1
         }
+        keep_prob = self.register_placeholder('keep_prob', shape=None, dtype=tf.float32)
+        lr = self.register_placeholder('lr', shape=None, dtype=tf.float32)
 
         self.input_tensor = tf.placeholder(tf.int8, [None, 300, 300, 3], name="input")
         cast_float = (tf.cast(self.input_tensor, tf.float32) - 128.0) / 128
-        conv1 = tf_learn.layers.conv2d(cast_float, depth=8, filter_size=3, strides=1, activation='relu')
-        conv1 = tf.concat(3, [cast_float, conv1])
-        pool1 = tf.nn.max_pool(conv1, [1, 3, 3, 1], [1, 3, 3, 1], padding='SAME')
-        conv2 = tf_learn.layers.conv2d(pool1, depth=32, filter_size=3, strides=1, activation='relu')
-        conv2 = tf.concat(3, [pool1, conv2])
-        pool2 = tf.nn.max_pool(conv2, [1, 3, 3, 1], [1, 3, 3, 1], padding='SAME')
-        conv3 = tf_learn.layers.conv2d(pool2, depth=128, filter_size=3, strides=1, activation='relu')
-        conv3 = tf.concat(3, [pool2, conv3])
-        pool3 = tf.nn.max_pool(conv3, [1, 3, 3, 1], [1, 3, 3, 1], padding='SAME')
-        conv4 = tf_learn.layers.conv2d(pool3, depth=512, filter_size=3, strides=1, activation='relu')
-        conv4 = tf.concat(3, [pool3, conv4])
-        pool4 = tf.nn.max_pool(conv4, [1, 3, 3, 1], [1, 3, 3, 1], padding='SAME')
-        lrn = tf.nn.local_response_normalization(pool4, name='local_response_normalization')
-        flattened = tf_learn.layers.flatten(lrn, name='flatten')
+        with tf.name_scope('layer1'):
+            conv1 = tf_learn.layers.conv2d(cast_float, depth=8, filter_size=3, strides=1, activation='relu')
+            lrn1 = tf.nn.local_response_normalization(conv1, name='local_response_normalization1')
+            stack1 = tf.concat(3, [cast_float, lrn1], name='stack1')
+            pool1 = tf.nn.max_pool(stack1, [1, 3, 3, 1], [1, 3, 3, 1], padding='SAME')
+        with tf.name_scope('layer2'):
+            conv2 = tf_learn.layers.conv2d(pool1, depth=32, filter_size=3, strides=1, activation='relu')
+            lrn2 = tf.nn.local_response_normalization(conv2, name='local_response_normalization2')
+            stack2 = tf.concat(3, [pool1, lrn2], name='stack2')
+            pool2 = tf.nn.max_pool(stack2, [1, 3, 3, 1], [1, 3, 3, 1], padding='SAME')
+        with tf.name_scope('layer3'):
+            conv3 = tf_learn.layers.conv2d(pool2, depth=128, filter_size=3, strides=1, activation='relu')
+            lrn3 = tf.nn.local_response_normalization(conv3, name='local_response_normalization3')
+            stack3 = tf.concat(3, [pool2, lrn3], name='stack3')
+            pool3 = tf.nn.max_pool(stack3, [1, 3, 3, 1], [1, 3, 3, 1], padding='SAME')
+        """
+        with tf.name_scope('layer4'):
+            conv4 = tf_learn.layers.conv2d(pool3, depth=512, filter_size=3, strides=1, activation='relu')
+            lrn4 = tf.nn.local_response_normalization(conv4, name='local_response_normalization4')
+            stack4 = tf.concat(3, [pool3, lrn4], name='stack4')
+            pool4 = tf.nn.max_pool(stack4, [1, 3, 3, 1], [1, 3, 3, 1], padding='SAME')
+        """
+        flattened = tf_learn.layers.flatten(pool3, name='flatten')
         fc1 = tf_learn.layers.fully_connection(flattened, 1024 * 2, activation='tanh', name='fc1')
-        keep_prob = self.register_placeholder('keep_prob', shape=None, dtype=tf.float32)
-        lr = self.register_placeholder('lr', shape=None, dtype=tf.float32)
         dropped1 = tf.nn.dropout(fc1, keep_prob, name='dropout1')
         fc2 = tf_learn.layers.fully_connection(dropped1, 1024 * 8, activation='tanh', name='fc2')
         dropped2 = tf.nn.dropout(fc2, keep_prob, name='dropout2')
