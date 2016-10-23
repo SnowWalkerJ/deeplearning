3486272a208eb1bf7bad242dc33cd40c3024d39c
SnowWalkerJ <jike3212001@163.com>
2016-10-23 13:16:09 +0800

    .

--- cat-dog/train.py
+++ cat-dog/train.py
@@ -1,6 +1,5 @@
 from tf_learn.utils.data_utils import LazyImageData, BigData
 from net import Model
-import os
 
 
 logdir = 'model1'
@@ -10,8 +9,6 @@ if __name__ == '__main__':
     data = BigData('dump')
     # data = LazyImageData('data/train', lambda name: 1 if name.split('.')[0] == 'cat' else 0)
     # data.set_crop((300, 300))
-    if not os.path.exists(logdir):
-        os.mkdir(logdir)
     model = Model(logdir)
     placeholders = {
         'keep_prob': {
