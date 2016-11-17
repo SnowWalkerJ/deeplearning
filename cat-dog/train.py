e89442920dd41d78957647a541d22879cf684efa
SnowWalkerJ <jike3212001@163.com>
2016-11-17 16:41:57 +0800

    .

--- cat-dog/train.py
+++ cat-dog/train.py
@@ -10,9 +10,10 @@ if __name__ == '__main__':
     # data = BigData('dump')
     # data = LazyImageData('data/train', lambda name: 1 if name.split('.')[0] == 'cat' else 0)
     # data = ImageData('data/train', lambda x: 1 if x.startswith("cat") else 0, handle=('resize', (300, 300)))
-    data = NpzData('dump/data.npz')
+    train_data = NpzData('dump/train.npz')
+    valid_data = NpzData('dump/validate.npz')
     # data.set_crop((300, 300))
     model = Model(logdir)
 
-    model.train(data, n_epoch=n_epoch, batch_size=100, validate=0.01, save_name='saves/easymodel')
+    model.train(train_data, n_epoch=n_epoch, batch_size=100, validate=valid_data, save_name='saves/easymodel')
 
